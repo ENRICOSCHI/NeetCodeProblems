53791a247b76cde42b4edfd4fd1113fee32548b2
ENRICOSCHI <113851247+ENRICOSCHI@users.noreply.github.com>
2023-02-02 18:02:24 +0100

    Added code in stack

--- Stack/Daily Temperatures/Daily Temperatures.cpp
+++ Stack/Daily Temperatures/Daily Temperatures.cpp
@@ -1,25 +1,17 @@
-/* 
-
-
-
-
-
-
-
-
-
-
-
-													THE CODE IS NOT READY YET
-
-
-
-
-
-
-
-
-
-
-
-*/
+class Solution {
+public:
+     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> s;
+        for (int i = 0; i < n; i++) {
+            while (!s.empty() && temperatures[s.top()] < temperatures[i]) {
+                int t = s.top();
+                s.pop();
+                ans[t] = i - t;
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+};
--- Stack/Evaluate Reverse Polish Notation/Evaluate Reverse Polish Notation.cpp
+++ Stack/Evaluate Reverse Polish Notation/Evaluate Reverse Polish Notation.cpp
@@ -1,20 +1,36 @@
-/* 
-
-
-
-
-
-
-
-
-													CODE NOT READY YET
-													
-													
-													
-													
-													
-
-
-
-
-*/
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        for (auto &t : tokens) {
+            if (t == "+") {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b + a);
+            } else if (t == "-") {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b - a);
+            } else if (t == "*") {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b * a);
+            } else if (t == "/") {
+                int a = s.top();
+                s.pop();
+                int b = s.top();
+                s.pop();
+                s.push(b / a);
+            } else {
+                s.push(stoi(t));
+            }
+        }
+        return s.top();
+    }
+};
